421df6fcf8fbfd01e6f7ef331fb7932f4489527b
chenrie <calhenrie@icloud.com>
2015-02-01 17:32:58 -0700

    Add files in usingvariable

---
+++ bmi calc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bmi calc
+//
+//  Created by Guest User on 1/30/15.
+//  Copyright (c) 2015 calhenrie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
